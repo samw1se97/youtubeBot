ccc8459413ecf9a65aec3a0392744110602e60ab
Ofek <ofek@sailo.tech>
2023-04-23 22:04:56 +0300

    botui

--- public/bot.js
+++ public/bot.js
@@ -1,38 +1,39 @@
-var botui = new Botui('my-botui-app');
+$(document).ready(function () {
+    var botui = new Botui('my-botui-app');
 
+    function enterText() {
+        botui.action.text({
+            action: {
+                placeholder: 'Enter your text'
+            }
+        }).then(function (res) {
 
-function enterText() {
-    botui.action.text({
-        action: {
-            placeholder: 'Enter your text'
-        }
-    }).then(function (res) {
+            console.log(res);
 
-        console.log(res);
-
-        $.ajax({
-            contentType: 'application/json',
-            data: JSON.stringify({text: res.value}),
-            dataType: 'json',
-            success: function(data){
-                botui.message.add({
-                    content: data.text
-                });
-
-                if (data.videoId) {
+            $.ajax({
+                contentType: 'application/json',
+                data: JSON.stringify({ text: res.value }),
+                dataType: 'json',
+                success: function (data) {
                     botui.message.add({
-                        type: 'embed',
-                        content: 'https://www.youtube.com/embed/' + data.videoId
+                        content: data.text
                     });
-                }
 
-                enterText();
-            },
-            type: 'POST',
-            url: '/youtube'
+                    if (data.videoId) {
+                        botui.message.add({
+                            type: 'embed',
+                            content: 'https://www.youtube.com/embed/' + data.videoId
+                        });
+                    }
+
+                    enterText();
+                },
+                type: 'POST',
+                url: '/youtube'
+            });
         });
-    });
 
-}
+    }
 
-enterText();
+    enterText();
+});
